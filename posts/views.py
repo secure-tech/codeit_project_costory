c9b103dcfad316251b3cc9003abe03b189ece1b3
js10081 <js10081008@gmail.com>
2022-01-24 14:46:21 +0900

    form 적용

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 
 def post_list(request):
@@ -11,3 +12,17 @@ def post_detail(request,post_id):
     post = Post.objects.get(id=post_id)
     cont = {'post':post}
     return render(request,'posts/post_detail.html',cont)
+
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        new_post = Post(
+            title = title,
+            content = content
+        )
+        new_post.save()
+        return redirect('post-detail',post_id=new_post.id)
+    else:
+        post_form = PostForm
+    return render(request,'posts/post_form.html',{'form':post_form})
