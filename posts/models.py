e6e45d34d703442b627248408390ba55772a1ae8
js10081 <js10081008@gmail.com>
2022-01-24 19:17:19 +0900

    유효성 검증 추가

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from .validators import validate_symbols
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField()
+    title = models.CharField(max_length=50, unique=True,
+    error_messages={'unique':'이미 있는 제목이네요!'},
+    validators=[validate_symbols]
+    )
+    content = models.TextField(validators=[MinLengthValidator(10,'너무 짧군요! 10자 이상 적어주세요.'),validate_symbols])
     dt_created = models.DateTimeField(verbose_name="Date Created",auto_now_add=True)
     dt_modified = models.DateTimeField(verbose_name="Date Modified",auto_now=True)
 
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from .models import Post
 from .forms import PostForm
+
 # Create your views here.
 
 def post_list(request):
@@ -16,8 +17,9 @@ def post_detail(request,post_id):
 def post_create(request):
     if request.method == 'POST':
         post_form = PostForm(request.POST)
-        new_post = post_form.save()
-        return redirect('post-detail',post_id=new_post.id)
+        if post_form.is_valid():
+            new_post = post_form.save()
+            return redirect('post-detail',post_id=new_post.id)
     else:
         post_form = PostForm
     return render(request,'posts/post_form.html',{'form':post_form})
