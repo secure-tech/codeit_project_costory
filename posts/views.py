f4294d78679c8e7d543c716f121a6db99d20cba3
js10081 <js10081008@gmail.com>
2022-01-25 14:45:12 +0900

    수정 및 삭제 구현

--- posts/views.py
+++ posts/views.py
@@ -26,11 +26,19 @@ def post_create(request):
 
 def post_update(request,post_id):
     post = Post.objects.get(id=post_id)
-    if request.method == 'POST':
+    if (request.method == 'POST'):
         post_form = PostForm(request.POST,instance=post)
-        if post_form.is_valid():
+        if (post_form.is_valid()):
             post_form.save()
             return redirect('post-detail',post_id=post.id)
     else:
         post_form = PostForm(instance=post)
     return render(request,'posts/post_form.html',{'form':post_form})
+
+def post_delete(request,post_id):
+    post = Post.objects.get(id=post_id)
+    if (request.method == 'POST'):
+        post.delete()
+        return redirect('post-list')
+    else:
+        return render(request,'posts/post_confirm_delete.html',{'post':post})
